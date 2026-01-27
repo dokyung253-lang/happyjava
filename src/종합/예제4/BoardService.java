e211ca827f7d49f898341958b62aefd977c9a946
dokyung253-lang <dokyung253-lang@gmail.com>
2026-01-27 15:22:28 +0900

    0127(7)

--- src/종합/예제4/BoardService.java
+++ src/종합/예제4/BoardService.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 // - 실행클래스 : 입출력담당
 public class BoardService {
     // -- 최초 실행 함수 = main
+    BoardController bc = new BoardController();
     public static void main(String[] args) {
         for( ; ; ){ // 반복문
             System.out.println("==================My Community===================");
@@ -18,15 +19,13 @@ public class BoardService {
                 String content = scan.nextLine();
                 System.out.println("작성자: ");
                 String writer = scan.nextLine();
-                // BoardController 의 저장기능 호출하여 저장결과 받기
-                BoardController bc = new BoardController();
-                boolean result = bc.doPost(content, writer)
+        // BoardController 의 저장기능 호출하여 저장결과 받기
+                boolean result = bc.doPost(content, writer);
                 if( result == true){System.out.println("[안내]저장 성공");}
                 else{System.out.println("[경고] 저장 실패");}
-
             }else if( ch ==2 ){
-                // BoardController 의 조회기능 호출하여 조회결과 받기
-                BoardController bc = new BoardController();
+        // BoardController 의 조회기능 호출하여 조회결과 받기
+
                 Board[ ]boards = bc.doget();
                 for( int index = 0; index <= boards.length -1; index++){
                     if(boards[index] != null){
