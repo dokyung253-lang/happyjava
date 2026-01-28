70cb84b1d59ddfe45b447a0c970b41e3abbffabb
dokyung253-lang <dokyung253-lang@gmail.com>
2026-01-28 16:02:12 +0900

    0127(10)

---
+++ src/day25/package1/A.java
@@ -0,0 +1,15 @@
+package day25.package1;
+
+public class A {
+    public int 공개변수;
+    private int 비공개변수;
+    int 일반변수; // default
+    public void 메소드(){ // ==== 같은 클래스 : 접근제한자 의미 없음 =====
+        System.out.println("공개변수 = "+ 공개변수);        // ok
+        System.out.println("비공개변수 = "+ 비공개변수);     // ok
+        System.out.println("일반변수 = "+ 일반변수);        // ok
+    }
+    private void 비공개메소드(){
+        System.out.println("A.비공개메소드"); // sout+m
+    }
+}
