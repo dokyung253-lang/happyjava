aa2ff82489f4a8a37c3875375a91b58ff90d046b
dokyung253-lang <dokyung253-lang@gmail.com>
2026-01-29 16:39:39 +0900

    0129(7)

--- src/종합/예제6/model/dao/BoardDao.java
+++ src/종합/예제6/model/dao/BoardDao.java
@@ -1,9 +1,29 @@
 package 종합.예제6.model.dao;
 
+import 종합.예제6.model.dto.BoardDto;
+
+import java.util.ArrayList;
+
 public class BoardDao {
     private BoardDao(){}
     private static  final BoardDao instance = new BoardDao();
     public static BoardDao getInstance(){
         return instance;
+    }
+
+    // [*] 데이터베이스 역할하는 ArrayList <추후에 삭제 예정>
+    private static final ArrayList<BoardDto> boards = new ArrayList<>();
+    // [*] 전역변수에 저장되니 최신 게시물번호 저장 < 식별키 생성 목적 : 추후에 삭제 예정 >
+    private static int currentNo = 1;
+
+    //  1) 등록 처리 : controller가 dao에게 content와 writer 전달하여 등록 요청한다.
+    public boolean doPost( String content,  String writer ){
+        // 1. 매개변수에 객체를 만든다. + 게시물번호 자동생성
+        BoardDto boardDto = new BoardDto( currentNo, content, writer);
+        // 2. .add( 새로운 값 )  : 저장하면 true 실패하면 false 반환하는 기능/함수/메소드
+        boolean saveResult = boards.add( boardDto );
+        currentNo++; // 다음 저장할 게시물의 번호를 1 증가한다.
+        // 3. 저장 성공 여부를 controller에게 반환한다.
+        return saveResult;
     }
 }
--- src/종합/예제6/model/dto/BoardDto.java
+++ src/종합/예제6/model/dto/BoardDto.java
@@ -1,4 +1,33 @@
 package 종합.예제6.model.dto;
 
 public class BoardDto {
+    // 1. 멤버변수 = 속성 = 테이블(표)와 일치 권장*
+    private int no;
+    private String content;
+    private String writer;
+
+    // 2. 생성자 : 관례적으로 기본생성자+ 풀생성자
+    public BoardDto(int currentNo, String content, String writer){}
+    public BoardDto(String writer, String content, int no) {
+        this.writer = writer;
+        this.content = content;
+        this.no = no;
+    }
+// 3. 메소드 : 관례적으로 private 멤버변수에 대해 setter/getter
+    public int getNo() {return no;}
+    public void setNo(int no) {this.no = no;}
+    public String getContent() {return content;}
+    public void setContent(String content) {this.content = content;}
+    public String getWriter() {return writer;}
+    public void setWriter(String writer) {this.writer = writer;}
+    // toString : 개발자를 위해
+
+    @Override
+    public String toString() {
+        return "BoardDto{" +
+                "no=" + no +
+                ", content='" + content + '\'' +
+                ", writer='" + writer + '\'' +
+                '}';
+    }
 }
