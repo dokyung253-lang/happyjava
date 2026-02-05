a53f169dab0b83a11181bf70c7248ee693956158
dokyung253-lang <dokyung253@gmail.com>
2026-02-05 16:40:01 +0900

    0205(04:16)

---
+++ src/day11/Exam3.java
@@ -0,0 +1,11 @@
+package day11;
+
+public class Exam3 {
+    public static void main(String[] args) {
+        // [ JDBC ] : 자바 데이터베이스 연동 라이브러리
+            // mysql-connector-j-9.6.0 jar 파일 준비한다.
+            // 준비된 .jar 파일을 프로젝트 폴더에 넣어준다.
+            // .jar 폴더 클릭
+
+    }// main end
+}// class end
